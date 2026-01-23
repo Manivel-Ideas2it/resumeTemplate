f77c2d09569b92be1b9818e3a3d8be4af018dc40
“Manivel-Ideas2it” <manivel.subramani@ideas2it.com>
2026-01-23 17:06:46 +0530

    todo crud functionalities

--- todo.js
+++ todo.js
@@ -1,3 +1,16 @@
+const listContainer = document.getElementById("task-list");
+const taskInput = document.getElementById("addTask");
+
 function handleSideBar() {
-    document.getElementById("sideBar").style.display = "none";
+    const sideBar = document.getElementById("sideBar");
+    sideBar.style.display = "none";
+}
+
+function handleChange(event) {
+    const task = event.target.value.trim();
+    const taskElement = document.createElement("p");
+    taskElement.className = "task-item";
+    taskElement.textContent = task;
+    listContainer.appendChild(taskElement);
+    event.target.value = "";
 }
